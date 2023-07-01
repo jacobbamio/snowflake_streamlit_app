f8af9982fe20fbe10f16f9b67c09eb3098f6b467
Jacob <119048387+jacobbamio@users.noreply.github.com>
2023-07-01 08:45:51 +0200

    forcing control flow issues just for checking

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,6 @@ add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 
 #Allow the end user to add a fruit to the list
 streamlit.write(f"Thanks for adding {add_my_fruit}")
+
+#This will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit'")
